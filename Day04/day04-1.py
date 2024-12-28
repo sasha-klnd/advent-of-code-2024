db914e5b6a019d3aad29dd24fd96b9d8fc471350
Sasha Klein-Charland <sp.kleincharland@gmail.com>
2024-12-27 21:34:06 -0500

    day04-1 solution

--- Day04/day04-1.py
+++ Day04/day04-1.py
@@ -1,86 +1,70 @@
 
 path = "Day04/input.txt"
 
-def process_input(path):
+def read_file(path):
     with open(path, "r") as file:
-        data = file.read()
 
-        rows = data.split("\n")
+        grid = []
 
-        for i in range(len(rows)):
-            cols = []
-            for letter in rows[i]:
-                cols.append(letter)
-            rows[i] = cols
-        return rows
+        linestr = file.readline()
+        
+        while linestr:
+            linestr = linestr.strip()
+            linearr = []
+            for letter in linestr:
+                linearr.append(letter)
 
-arr = process_input(path)
-width = len(arr[0])
-height = len(arr)
+            grid.append(tuple(linearr))
+            linestr = file.readline()
 
-def search_surrounding(arr, i, j):
-    # Call when X is found
-    width = len(arr[0])
-    height = len(arr)
+        return grid
 
-    directions = {}
-    matches = []
+def count_xmas(arr):
 
-    directions["t"] = arr[i-1][j] if (i > 2) else '0'
-    directions["tr"] = arr[i-1][j+1] if (i > 2 and j < (width-3)) else '0'
-    directions['r'] = arr[i][j+1] if j < (width-3) else '0'
-    directions['br'] = arr[i+1][j+1] if (i < (height-3) and j < (width-3)) else '0'
-    directions['b'] = arr[i+1][j] if (i < (height-3)) else '0'
-    directions['bl'] = arr[i+1][j-1] if (i < (height-3) and j > 2) else '0'
-    directions['l'] = arr[i][j-1] if (j > 2) else '0'
-    directions['tl'] = arr[i-1][j-1] if (i > 2 and j > 2) else '0'
+    count = 0
 
-    for direction in tuple(directions.keys()):
-        if directions[direction] == "M":
-            matches.append(direction)
-    
-    return matches
+    height = len(arr) - 1
+    width = len(arr[0]) - 1
 
-def full_search(arr, i, j, directions):
-    matches = 0
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            if arr[i][j] == "X":
+                # Check each direction fully
+                # Up
+                if i >= 3:
+                    if (arr[i-1][j] == "M") and (arr[i-2][j] == "A") and (arr[i-3][j] == "S"):
+                        count += 1
+                # Up Right
+                if i >= 3 and j <= (width - 3):
+                    if (arr[i-1][j+1] == "M") and (arr[i-2][j+2] == "A") and (arr[i-3][j+3] == "S"):
+                        count += 1
+                # Right
+                if j <= (width - 3):
+                    if (arr[i][j+1] == "M") and (arr[i][j+2] == "A") and (arr[i][j+3] == "S"):
+                        count += 1
+                # Down Right
+                if i <= (height - 3) and j <= (width - 3):
+                    if (arr[i+1][j+1] == "M") and (arr[i+2][j+2] == "A") and (arr[i+3][j+3] == "S"):
+                        count += 1
+                # Down
+                if i <= (height - 3):
+                    if (arr[i+1][j] == "M") and (arr[i+2][j] == "A") and (arr[i+3][j] == "S"):
+                        count += 1  
+                # Down Left
+                if i <= (height - 3) and j >= 3:
+                    if (arr[i+1][j-1] == "M") and (arr[i+2][j-2] == "A") and (arr[i+3][j-3] == "S"):
+                        count += 1
+                # Left
+                if j >= 3:
+                    if (arr[i][j-1] == "M") and (arr[i][j-2] == "A") and (arr[i][j-3]== "S"):
+                        count += 1
+                # Up Left
+                if i >= 3 and j >= 3:
+                    if (arr[i-1][j-1] == "M") and (arr[i-2][j-2] == "A") and (arr[i-3][j-3] == "S"):
+                        count += 1
 
-    for direction in directions:
-        if direction == "t":
-            if arr[i][j] == "X" and arr[i-1][j] == "M" and arr[i-2][j] == "A" and arr[i-3][j] == "S":
-                matches += 1
-        elif direction == "tr":
-            if arr[i][j] == "X" and arr[i-1][j+1] == "M" and arr[i-2][j+2] == "A" and arr[i-3][j+3] == "S":
-                matches += 1
-        elif direction == "r":
-            if arr[i][j] == "X" and arr[i][j+1] == "M" and arr[i][j+2] == "A" and arr[i][j+3] == "S":
-                matches += 1
-        elif direction == "br":
-            if arr[i][j] == "X" and arr[i+1][j+1] == "M" and arr[i+2][j+2] == "A" and arr[i+3][j+3] == "S":
-                matches += 1
-        elif direction == "b":
-            if arr[i][j] == "X" and arr[i+1][j] == "M" and arr[i+2][j] == "A" and arr[i+3][j] == "S":
-                matches += 1
-        elif direction == "bl":
-            if arr[i][j] == "X" and arr[i+1][j-1] == "M" and arr[i+2][j-2] == "A" and arr[i+3][j-3] == "S":
-                matches += 1
-        elif direction == "l":
-            if arr[i][j] == "X" and arr[i][j-1] == "M" and arr[i][j-2] == "A" and arr[i][j-3] == "S":
-                matches += 1
-        elif direction == "tl":
-            if arr[i][j] == "X" and arr[i-1][j-1] == "M" and arr[i-2][j-2] == "A" and arr[i-3][j-3] == "S":
-                matches += 1
+    return count
 
-    return matches
-
-
-for i in range(height):
-    for j in range(width):
-        if arr[i][j] == "X":
-            directions = search_surrounding(arr, i, j)
-
-            if directions:
-                matches = full_search(arr, i, j, directions)
-
-
-
-print(matches)
+grid = read_file(path)
+count = count_xmas(grid)
+print(count)
